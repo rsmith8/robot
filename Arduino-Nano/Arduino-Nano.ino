0148aac64a0148284b512777b284550a95da76c3
rsmith8 <raasmith@mtu.edu>
2025-01-28 08:31:23 -0500

    Update Arduino-Nano.ino
    Added ToF through Setup

--- Arduino-Nano/Arduino-Nano.ino
+++ Arduino-Nano/Arduino-Nano.ino
@@ -1,47 +1,57 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// Released under the GPLv3 license to match the rest of the
-// Adafruit NeoPixel library
+/*----Code for Arduino-Nano with pin layout------------
+ Top Left
+  --
+  Pin 4 GND                             (Wired)
+  PIN D6 Sonar REAR ECHO                (Wired)
+  PIN D8 Sonar  FL ECHO                 (Wired)
+  PIN D9 Sonar  FL TRIG                 (Wired)
+  PIN D10 Neo                           (Wired) 0)RF-Bumper 1)LF-Bumper 2)Top (G,R,B)
+  PIN D11 Sonar FR ECHO                 (Wired)
+  PIN D12 Sonar FR TRIG                 (Wired)
+  --
+ USB
+  --
+  PIN D13 Sonar REAR TRIG               (Wired)
+  PIN A0/D14 PWM Left/Throttle          (wired)
+  PIN A1/D15 PWM Right/Steer            (wired)
+  PIN A2/D16 PWM Dump			              (wired)
+  PIN A4 ToF SDA I2C bus at 115200 baud (wired)
+  PIN A5 ToF SCL I2C bus at 115200 baud (wired)
+  PIN 29 GND                            (Wired)
+  PIN 30 +5V (Vin 7-12V move to 27? +5) (wired)
+  --
+ Top Right
+
+ Missing and in code:
+  PIN 2 Neopixel                        empty
+  PIN 7 LED left indicator              ?Something on pin
+  PIN 8 LED right indicatorn            ?Something on pin
+  PIN A7 Voltage divider                empty
+  PWM Pins 3, 5, 6, 9, 10, 11
+------------------------------------------------*/
+
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-#define PIN        10 // On Trinket or Gemma, suggest changing this to 1
-
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS 3 // Popular NeoPixel ring size
-
-// When setting up the NeoPixel library, we tell it how many pixels,
-// and which pin to use to send signals. Note that for older NeoPixel
-// strips you might need to change the third parameter -- see the
-// strandtest example for more information on possible values.
+byte deviceAddress = 0x10;  // The address of the TF-Luna device is 0x10
+#define PIN 10              //Neopixel is attached to  
+#define NUMPIXELS   3       // number of neopixels in strip  
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-
-#define DELAYVAL 1000 // Time (in milliseconds) to pause between pixels
-
-const int leftPwmPin = 14;  // PWM output for left joystick is 14 on yardbot - test board is 6
-const int rightPwmPin = 15; // PWM output for right joystick is 15 on yardbot - test board is 6
-const int dumpPwmPin = 16; // PWM output for right joystick is 16 on yardbot - test board is 6
-
+int delayval = 100;         // timing delay in milliseconds pause between pixels 
+unsigned int distance = 0;  // counts connected arduino and stadia devices
 int counter=0;
 int alivecount=0;
 
-Servo leftPWM;
+const int leftPwmPin = 14;  // PWM output for left joystick is 14 on yardbot - test board is 6
+const int rightPwmPin = 15; // PWM output for right joystick is 15 on yardbot - test board is 6
+const int dumpPwmPin = 16;  // PWM output for right joystick is 16 on yardbot - test board is 6
+Servo leftPWM;   // create servo object to control a servo
 Servo rightPWM;  // create servo object to control a servo
-Servo dumpPWM;
+Servo dumpPWM;   // create servo object to control a servo
 
 void setup() {
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-    clock_prescale_set(clock_div_1);
-  #endif
-  // END of Trinket-specific code.
-
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  
+  Wire.begin();             // The I2C bus communication starts
   Serial.begin(115200);
   leftPWM.attach(leftPwmPin);  // attaches the servo on pin to the servo object
   rightPWM.attach(rightPwmPin);  // attaches the servo on pin to the servo object
@@ -49,7 +59,6 @@ void setup() {
   pixels.setPixelColor(0, pixels.Color(0, 50, 0)); //GRB
   pixels.setPixelColor(1, pixels.Color(0, 50, 0)); //GRB
   pixels.setPixelColor(2, pixels.Color(0, 50, 0)); //GRB
-
 }
 
 void loop() {
