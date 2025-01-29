6ff9fddc1b28eb055ba60a791eb311be195c1584
rsmith8 <raasmith@mtu.edu>
2025-01-28 21:38:10 -0500

    Added ToF
    to working Arduino-Nano code

--- Arduino-Nano/Arduino-Nano.ino
+++ Arduino-Nano/Arduino-Nano.ino
@@ -41,75 +41,115 @@ int delayval = 100;         // timing delay in milliseconds pause between pixels
 unsigned int distance = 0;  // counts connected arduino and stadia devices
 int counter=0;
 int alivecount=0;
+int range_stop=15;
+int range_close=45;
+
 
 const int leftPwmPin = 14;  // PWM output for left joystick is 14 on yardbot - test board is 6
 const int rightPwmPin = 15; // PWM output for right joystick is 15 on yardbot - test board is 6
-const int dumpPwmPin = 16;  // PWM output for right joystick is 16 on yardbot - test board is 6
+const int dumpPwmPin = 16;  // PWM output for dumping is 16 on yardbot - test board is 6
 Servo leftPWM;   // create servo object to control a servo
 Servo rightPWM;  // create servo object to control a servo
 Servo dumpPWM;   // create servo object to control a servo
 
 void setup() {
-  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-  Wire.begin();             // The I2C bus communication starts
-  Serial.begin(115200);
-  leftPWM.attach(leftPwmPin);  // attaches the servo on pin to the servo object
-  rightPWM.attach(rightPwmPin);  // attaches the servo on pin to the servo object
-  dumpPWM.attach(dumpPwmPin);  // attaches the servo on pin to the servo object
+  pixels.begin();               //INITIALIZE NeoPixel strip object (REQUIRED)
+  Wire.begin();                 //The I2C bus communication starts for ToF sensor
+  Serial.begin(115200);         //Start serial communication with Pi
+  leftPWM.attach(leftPwmPin);   //Attaches the servo on pin to the servo object
+  rightPWM.attach(rightPwmPin); //Attaches the servo on pin to the servo object
+  dumpPWM.attach(dumpPwmPin);   //Attaches the servo on pin to the servo object
   pixels.setPixelColor(0, pixels.Color(0, 50, 0)); //GRB
   pixels.setPixelColor(1, pixels.Color(0, 50, 0)); //GRB
   pixels.setPixelColor(2, pixels.Color(0, 50, 0)); //GRB
 }
 
 void loop() {
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-      
+  Wire.beginTransmission(deviceAddress);  //The I2C data transmission starts
+  Wire.write(0x00);                       //Send command
+  Wire.endTransmission();                 //The I2C data transfer is complete
+  Wire.requestFrom((uint8_t)deviceAddress, (uint8_t)7); //Read 7 bytes of data 
+
+  if (Wire.available() == 7) { //ToF Sensor - 7 bytes of data are available
+    byte data[7];
+    for (int i = 0; i < 7; i++) {
+      data[i] = Wire.read(); //Read data into an array
+    }
+    distance = (data[1] << 8) | data[0]; //Distance Value
+    unsigned int signalStrength = (data[3] << 8) | data[2]; //Signal Strength
+  }
 
   if (Serial.available()) {
     String input = Serial.readStringUntil('\n');
-      pixels.setPixelColor(0, pixels.Color(100, 0, 0)); //GRB
-    
-    if (input.startsWith("L")) {
+        
+    if (input.startsWith("L")) { //Left Joystick Input
       int leftY = input.substring(1).toInt();
-      int leftPwmValue = map(leftY, 0, 255, 135, 45); // Map joystick value to PWM range
+      if (distance<range_close){
+        if (leftY>128){
+          int leftPwmValue = 90;
+        }
+      else if (distance<=range_close){
+        int leftPwmValue = map(leftY, 0, 255, 125, 55); // Map joystick value to PWM range 180-0 full range
+      }
+      else{
+        int leftPwmValue = map(leftY, 0, 255, 135, 45); // Map joystick value to PWM range 180-0 full range
+      }
       leftPWM.write(leftPwmValue);
       analogWrite(leftPwmPin, leftPwmValue);
       pixels.setPixelColor(0, pixels.Color(0, 0, 100)); //GRB
       alivecount=0;
     } 
-    else if (input.startsWith("R")) {
+    else if (input.startsWith("R")) { //Right Joystick Input
       int rightY = input.substring(1).toInt();
-      int rightPwmValue = map(rightY, 0, 255, 45, 135); // Map joystick value to PWM range
+      if (distance<range_close){
+        if (rightY>128){
+          int rightPwmValue = 90;
+        }
+      else if (distance<=range_close){
+        int rightPwmValue = map(rightY, 0, 255, 55, 125); // Map joystick value to PWM range 180-0 full range
+      }
+      else{
+        int rightPwmValue = map(rightY, 0, 255, 45, 135); // Map joystick value to PWM range 180-0 full range
+      }
       rightPWM.write(rightPwmValue);
-      pixels.setPixelColor(1, pixels.Color(0, 0, 100)); //GRB
+      pixels.setPixelColor(1, pixels.Color(0, 0, 100)); //GRB using Blue to indicate controller input
       alivecount=0;
     }
-    else if (input.startsWith("D")) {
+    else if (input.startsWith("D")) { //Right Trigger Input
       int lower_dump = input.substring(1).toInt();
-      int DumpPwmValue = map(lower_dump, 0, 255, 90, 15); // Map joystick value to PWM range
+      int DumpPwmValue = map(lower_dump, 0, 255, 90, 15); // Map joystick value to PWM range 90-0 full range
       dumpPWM.write(DumpPwmValue);
-      pixels.setPixelColor(2, pixels.Color(0, 0, 100)); //GRB
+      pixels.setPixelColor(2, pixels.Color(0, 0, 100)); //GRB using Blue to indicate controller input
       alivecount=0;
     }
-    else if (input.startsWith("U")) {
+    else if (input.startsWith("U")) { //Left Trigger Input
       int raise_dump = input.substring(1).toInt();
-      int RaisePwmValue = map(raise_dump, 0, 255, 90, 165); // Map joystick value to PWM range
+      int RaisePwmValue = map(raise_dump, 0, 255, 90, 165); // Map joystick value to PWM range 90-180 full range
       dumpPWM.write(RaisePwmValue);
-      pixels.setPixelColor(2, pixels.Color(0, 0, 100)); //GRB
+      pixels.setPixelColor(2, pixels.Color(0, 0, 100)); //GRB using Blue to indicate controller input
       alivecount=0;
     }
-
-
   }
-  else {
-      pixels.setPixelColor(2, pixels.Color(50, 50, 0)); //GRB
+  
+  if (distance<=range_stop){ //Too Close - Stop
+    pixels.setPixelColor(2, pixels.Color(0, 200, 0));  //green, red, blue
   }
-  if (alivecount > 1000){
-      pixels.setPixelColor(0, pixels.Color(50, 50, 50)); //GRB
-      pixels.setPixelColor(1, pixels.Color(50, 50, 50)); //GRB    
+  else if (distance<=range_close){ //Caution slow down
+    pixels.setPixelColor(2, pixels.Color(200, 200, 0));  //green, red, blue
+  }
+  else if (distance > range_close){ //Safe Range
+    pixels.setPixelColor(2, pixels.Color(200, 0, 0));  //green, red, blue
+  }
+  else{ //No distance set to on/ready
+    pixels.setPixelColor(2, pixels.Color(100, 100, 100));  //green, red, blue
   }
 
-  if (counter > 10){
+  if (alivecount > 1000){ //After a while no input reset to white signal
+      pixels.setPixelColor(0, pixels.Color(50, 50, 50)); //GRB using White to illuminate while indicating on/ready 
+      pixels.setPixelColor(1, pixels.Color(50, 50, 50)); //GRB using White to illuminate while indicating on/ready  
+  }
+
+  if (counter > 10){ //counter used to implement recommended delay for sending light updates 
     pixels.show();   // Send the updated pixel colors to the hardware.
     counter=0;
   }    
